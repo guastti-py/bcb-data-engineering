7a8754dfcc875d0c840c92f6e8d8d592f80b942e
Gabriel Almeida <guastti@gmail.com>
2026-09-14 17:53:25 -0300

    Corrige fluxo de carga do PostgreSQL

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ from database.carregar_selic_silver import carregar_selic_silver
 from database.carregar_dolar_silver import carregar_dolar_silver
 
 
+# =========================
+# SELIC
+# =========================
+
 arquivo_bronze, teve_atualizacao = extrair_selic()
 
 if teve_atualizacao:
@@ -15,21 +19,28 @@ if teve_atualizacao:
     arquivo_silver = transformar_silver(
         arquivo_bronze
     )
+
 else:
-    
-    print("selic sem dados. "
-          "Utilizando a Silver existente."
+
+    print(
+        "Selic sem novos dados. "
+        "Utilizando a Silver existente."
     )
-    
+
     arquivo_silver = "data/silver/selic_historico.parquet"
-    
-    carregar_selic_silver(
-        arquivo_silver
-    )
 
-    print("Pipeline da Selic concluído até a camada Silver")
-    print(arquivo_silver)
 
+carregar_selic_silver(
+    arquivo_silver
+)
+
+print("Pipeline da Selic concluído com sucesso")
+print(arquivo_silver)
+
+
+# =========================
+# DÓLAR
+# =========================
 
 arquivo_bronze_dolar, teve_atualizacao_dolar = extrair_dolar()
 
@@ -38,18 +49,20 @@ if teve_atualizacao_dolar:
     arquivo_silver_dolar = transformar_silver_dolar(
         arquivo_bronze_dolar
     )
-    
+
 else:
-    
-    print("Dólar sem novos dados. "
-          "Utilizando a Silver existente."
-    )
-    
-    arquivo_silver_dolar = "data/silver/dolar_historico.parquet"
-    
-    carregar_dolar_silver(
-        arquivo_silver_dolar
+
+    print(
+        "Dólar sem novos dados. "
+        "Utilizando a Silver existente."
     )
 
-    print("Pipeline do Dólar concluído até a camada Silver")
-    print(arquivo_silver_dolar)
+    arquivo_silver_dolar = "data/silver/dolar_historico.parquet"
+
+
+carregar_dolar_silver(
+    arquivo_silver_dolar
+)
+
+print("Pipeline do Dólar concluído com sucesso")
+print(arquivo_silver_dolar)
